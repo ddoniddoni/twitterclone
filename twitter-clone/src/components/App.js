b8f2cd42d6b0d75fa57cb28e98af859cbccc90fe
ddoniddoni <psdkei@naver.com>
2020-09-28 18:32:58 +0900

    firebase Auth

--- twitter-clone/src/components/App.js
+++ twitter-clone/src/components/App.js
@@ -1,8 +1,16 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggednIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggednIn} />
+      <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+    </>
+  );
 }
 
 export default App;
