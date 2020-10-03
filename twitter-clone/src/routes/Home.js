d8a03754eb6afa5058704c315fe7d24737fec5bd
ddoniddoni <psdkei@naver.com>
2020-10-03 17:04:23 +0900

    Read FireStore

--- twitter-clone/src/routes/Home.js
+++ twitter-clone/src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from "fbase";
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("tweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -19,7 +34,6 @@ const Home = () => {
     } = e;
     setNweet(value);
   };
-
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -32,6 +46,13 @@ const Home = () => {
         />
         <input type="submit" value="New" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
