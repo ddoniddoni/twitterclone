347483066ff249ae6807091cee95fd8b09eb652b
ddoniddoni <psdkei@naver.com>
2020-10-03 16:19:55 +0900

    Update FireStore

--- twitter-clone/src/routes/Home.js
+++ twitter-clone/src/routes/Home.js
@@ -1,10 +1,16 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await dbService.collection("tweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
   };
 
   const onChange = (e) => {
