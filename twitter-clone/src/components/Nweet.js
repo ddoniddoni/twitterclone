96ab9a6602bbf5fefd295c83d287bfbc0d2f93d3
ddoniddoni <psdkei@naver.com>
2020-10-06 17:12:50 +0900

    Delete and Update

--- twitter-clone/src/components/Nweet.js
+++ twitter-clone/src/components/Nweet.js
@@ -1,13 +1,61 @@
+import { dbService } from "fbase";
 import React from "react";
+import { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure wnat to delete this?");
+    console.log(ok);
+    if (ok) {
+      await dbService.doc(`tweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`tweets/${nweetObj.id}`).update({
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Nweet</button>
-          <button>Edit Nweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="edit your text"
+              vlaue={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
         </>
       )}
     </div>
