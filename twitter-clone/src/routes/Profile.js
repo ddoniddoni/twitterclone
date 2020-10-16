f57f48aa25a74cfcf89e53983fc723162490bedf
ddoniddoni <psdkei@naver.com>
2020-10-16 20:34:18 +0900

    Get My Own Tweets

--- twitter-clone/src/routes/Profile.js
+++ twitter-clone/src/routes/Profile.js
@@ -1,13 +1,24 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 import React from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid);
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
