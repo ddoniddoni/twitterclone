d644946a4a365fc5cb84eb91744f19b00bcfd70b
ddoniddoni <psdkei@naver.com>
2020-10-17 17:18:57 +0900

    Add Edit Profile DisplayName

--- twitter-clone/src/routes/Profile.js
+++ twitter-clone/src/routes/Profile.js
@@ -1,13 +1,30 @@
 import { authService, dbService } from "fbase";
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
   };
 
   const getMyNweets = async () => {
@@ -25,6 +42,15 @@ const Profile = ({ userObj }) => {
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display Name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Logout</button>
     </>
   );
